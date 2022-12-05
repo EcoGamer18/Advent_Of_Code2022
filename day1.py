bc3bd9b5ee81e068a0169797736ea69571d32145
Gabriela T <54414302+EcoGamer18@users.noreply.github.com>
2022-12-05 08:07:47 +0200

    Day 5 done

---
+++ day1.py
@@ -0,0 +1,24 @@
+def read_from_file_1():
+    with open("input_day1.txt") as file:
+        elfs = []
+        elf = []
+        for line in file:
+            if line.strip():
+                elf.append(int(line.rstrip()))
+            else:
+                elfs.append(elf)
+                elf = []
+    return elfs
+
+
+def solution_1(arr):
+    return max(list(map(lambda x: sum(x), arr)))
+
+
+def solution_2(arr):
+    return sum(sorted(list(map(lambda x: sum(x), arr)), reverse=True)[:3])
+
+
+def print_day1():
+    print(solution_1(read_from_file_1()))
+    print(solution_2(read_from_file_1()))
---
+++ main.py
@@ -0,0 +1,11 @@
+import day1
+import day2
+import day3
+import day4
+import day5
+
+# day1.print_day1()
+# day2.print_day2()
+# day3.print_day3()
+# day4.print_day4()
+day5.print_day5()
