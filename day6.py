0bbfb0d69ff70e938282704f2a14630fac2cdeae
Gabriela T <54414302+EcoGamer18@users.noreply.github.com>
2022-12-06 07:13:54 +0200

    Day 6 done

---
+++ day6.py
@@ -0,0 +1,28 @@
+def read_from_file_6():
+    with open("input_day6.txt") as file:
+        for line in file:
+            tasks = line.strip()
+    return tasks
+
+
+def solution_1(arr):
+    marker = arr[:4]
+    for i in range(1, len(arr)):
+        if len(set(marker)) == 4:
+            return i + 3
+        marker = arr[i:i + 4]
+    return 0
+
+
+def solution_2(arr):
+    marker = arr[:14]
+    for i in range(1, len(arr)):
+        if len(set(marker)) == 14:
+            return i + 13
+        marker = arr[i:i + 14]
+    return 0
+
+
+def print_day6():
+    print(solution_1(read_from_file_6()))
+    print(solution_2(read_from_file_6()))
--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import day2
 import day3
 import day4
 import day5
+import day6
+
 
 # day1.print_day1()
 # day2.print_day2()
 # day3.print_day3()
 # day4.print_day4()
-day5.print_day5()
+# day5.print_day5()
+day6.print_day6()
